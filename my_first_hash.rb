7427bbb6980ee33c2d4590ab0b73b618d42cf174
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-24 18:39:25 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,16 +1,21 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  {"artists" => "Shakira", "Bomba Stereo",
-  "songs" => "Antologia", "Soy yo" }
+  def my_hash
+    fav_artist = {
+    "artists" => "Shakira",
+    "songs" => "Antologia"
+  }
 
-end
+  end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 
 end
 
