9651f9e5eb0a04eac9e63990ccd4b89f47d9ca38
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-24 18:43:18 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -38,12 +38,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest{"muskets" => 2}
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest{"gun powder" => 4}
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
